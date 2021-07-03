e57fe9cd589967d81e8a0bf5a74a9a0ba5700927
x0s3 <x0s3.prog@gmail.com>
2021-07-03 13:47:17 +0200

    - Traverse AST and find values for the given generic value to mocki fn

--- src/mocki.ts
+++ src/mocki.ts
@@ -2,6 +2,10 @@ import type * as babelTypes from '@babel/types';
 import type { Babel, MockiPlugin } from './types';
 import { createObject, createObjectProperty } from './utils/createObject';
 
+function isNotTS(name: string): boolean {
+  return name.endsWith('.js') || name.endsWith('.jsx');
+}
+
 export default function (babel: Babel): MockiPlugin {
   const { types: t } = babel;
   const mockiFnName = 'mocki';
@@ -9,31 +13,70 @@ export default function (babel: Babel): MockiPlugin {
   return {
     name: 'mocki-transformer',
     visitor: {
-      CallExpression(path) {
-        // @ts-ignore prop `name` exists
-        if (path.node.callee.name !== mockiFnName) return;
+      Program: {
+        enter(programPath, state) {
+          // @ts-ignore
+          const fileName = state.file.opts.filename;
 
-        let mockedObject = null;
-        const [mockiArgs] = path.node?.arguments;
+          if (isNotTS(fileName)) {
+            return;
+          }
 
-        if (mockiArgs) {
-          // @ts-ignore prop `properties` exists
-          const props = mockiArgs.properties?.map(
-            (prop: babelTypes.ObjectProperty) =>
-              createObjectProperty(
-                t,
-                // @ts-ignore prop `name` exists
-                prop.key.name,
-                // @ts-ignore prop `value` exists
-                prop.value.value,
-                prop.value.type
-              )
-          );
+          programPath.traverse({
+            CallExpression(callPath) {
+              // @ts-ignore prop `name` exists
+              if (callPath.node.callee.name !== mockiFnName) return;
 
-          mockedObject = createObject(t, props);
-        }
+              let mockedObject = null;
+              const [mockiArgs] = callPath.node?.arguments;
+              const [typeParameters] = callPath.node.typeParameters?.params!;
+              // @ts-ignore
+              const mockiGenericName = typeParameters.typeName.name;
 
-        path.replaceWith(mockedObject!);
+              if (t.isTSTypeReference(typeParameters)) {
+                programPath.traverse({
+                  TSInterfaceDeclaration(identifierPath) {
+                    const interfaceName = identifierPath.node.id.name;
+
+                    if (interfaceName !== mockiGenericName) return;
+
+                    identifierPath.traverse({
+                      TSPropertySignature(interfacePath) {
+                        const [contract] = interfacePath.container as object[];
+                        // @ts-ignore
+                        const attributeName = contract.key.name;
+                        const attributeValueType =
+                          // @ts-ignore
+                          contract.typeAnnotation.typeAnnotation;
+
+                        const a = t.isTSStringKeyword(attributeValueType);
+                      },
+                    });
+                  },
+                });
+              }
+
+              if (mockiArgs) {
+                // @ts-ignore prop `properties` exists
+                const props = mockiArgs.properties?.map(
+                  (prop: babelTypes.ObjectProperty) =>
+                    createObjectProperty(
+                      t,
+                      // @ts-ignore prop `name` exists
+                      prop.key.name,
+                      // @ts-ignore prop `value` exists
+                      prop.value.value,
+                      prop.value.type
+                    )
+                );
+
+                mockedObject = createObject(t, props);
+              }
+
+              callPath.replaceWith(mockedObject!);
+            },
+          });
+        },
       },
     },
   };
